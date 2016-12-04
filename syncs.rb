e9f49b1b398e68d486c7c1da7dd15155dc362c72
tsukushi1217 <tsukushi129@gmail.com>
2016-12-04 23:11:36 +0900

    ls

--- syncs.rb
+++ syncs.rb
@@ -49,13 +49,13 @@ hostname = `hostname`.chomp
 file_size = `du -s ./db/`.split("\t")[0]
 cpu_temp = `cat /sys/class/thermal/thermal_zone0/temp`.chomp
 now = Time.now.to_i.to_s
-File.open("./db/#{hostname}.log", "a"){|f|
+File.open("./status/#{hostname}.log", "a"){|f|
 
         f.puts [now, cpu_temp, file_size].join(" ")
 
 }
 
 
-system("rsync -uvrz --inplace -e 'ssh -i #{Config::SSHKey}'./db/ #{rdir}")
-
+system("rsync -uvrz --inplace -e 'ssh -i #{Config::SSHKey}' ./db/ #{rdir}")
+system("rsync -uvrz --inplace -e 'ssh -i #{Config::SSHKey}' ./status/ #{Config::MasterStatus}")
 end
